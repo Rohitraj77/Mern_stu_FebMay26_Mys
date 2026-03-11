4868cfbbdc6d021ad0b5253f91eca942b1eb174c
Rohit Raj <rohitrajm254@gmail.com>
2026-03-11 16:35:19 +0530

    Arithmetic operators

---
+++ w3/d2/p1.js
@@ -0,0 +1,14 @@
+//Arithmetic operators
+//LTS stands for long term support
+console.log("Arithmetic operators");
+const a=10,b=3;
+console.log("a+b",a+b);
+console.log("a-b",a-b);
+console.log("a*b",a*b);
+console.log("a/b",a/b);
+console.log("a%b",a%b);//Modulus
+console.log("a**b",a**b);//Exponential
+console.log("a++",a++);//post-increment
+console.log("++a",++a);//pre-increment
+console.log("a--",a--);//post-decrement
+console.log("--a",--a);//pre-decrement
