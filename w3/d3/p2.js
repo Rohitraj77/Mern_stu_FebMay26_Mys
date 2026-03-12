12119d60142d1cc4dd07a9a6ae54cb9d2667ffdf
Rohit Raj <rohitrajm254@gmail.com>
2026-03-12 14:05:36 +0530

    while loop

---
+++ w3/d3/p2.js
@@ -0,0 +1,16 @@
+// while loop
+let count = 0;
+while(count<5){
+    console.log("count: ",count);
+    count++;
+}
+
+console.log("do while")
+
+//do while
+// it gets exceuted atleast one irrespective of the condition.
+let value=10;
+do{
+    console.log("Value :",value);
+    value++;
+}while(value<3);
