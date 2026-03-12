f1907bef755258b81752b2039044d2d004edbcce
Rohit Raj <rohitrajm254@gmail.com>
2026-03-12 11:47:33 +0530

    conditional statements and type conversion

---
+++ w3/d2/p4.js
@@ -0,0 +1,33 @@
+// //conditional statements
+// let age=20;
+// if(age<13){
+//     console.log("child")
+// }
+// else if(age<18){
+//     console.log("Teenager")
+// }
+// else{
+//     console.log("Adult")
+// }
+
+// // switch statements
+// console.log("Conitional statements");
+// const day='M';
+// switch(day){
+//     case "Monday":
+//         console.log("Start of the week");
+//         break;
+//     case "Wednesday":
+//         console.log("Mid of the week");
+//         break;
+//     case "Friday":
+//         console.log("End of the work week");
+//         break;
+//     default:
+//         console.log("Some day in the week");
+//         break;
+// }
+
+//Type conversion
+let n=Number("ABC");
+console.log("n: ",n,"Type of n:",typeof n,"isNaN",isNaN(n));
