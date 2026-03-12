ef27ae6b9bd139142c8c4678f2c1bc85c378e182
Rohit Raj <rohitrajm254@gmail.com>
2026-03-12 11:46:12 +0530

    Logical operator

---
+++ w3/d2/p3.js
@@ -0,0 +1,6 @@
+//Logical operators
+let a=true,b=false;
+console.log("Logical operators");
+console.log("a&&b=",a&&b);//Logical AND
+console.log("a||b=",a||b);//Logical OR
+console.log("!a=",!a);//Logical NOT
