95fe61f2b8791787843101b6e4738b3605eec661
Rohit Raj <rohitrajm254@gmail.com>
2026-03-12 11:48:18 +0530

    Function decleration

---
+++ w3/d2/p5.js
@@ -0,0 +1,9 @@
+//Function Decaration
+// function greet(name);
+greet("Rohit");
+// Function Declaration/defination
+function greet(name){
+console.log("Hello,"+name+" !")
+}
+//Function
+greet("Rohit");
