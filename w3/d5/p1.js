edd8bf24f2083f39a2aa0623cd62a18d05b25432
Rohit Raj <rohitrajm254@gmail.com>
2026-03-12 14:17:44 +0530

    Basics click event js file

---
+++ w3/d5/p1.js
@@ -0,0 +1,10 @@
+// Basics of Click event
+
+const button = document.getElementById("clickBtn");
+button.addEventListener("click",function(){
+    console.log("Button is clicked");
+});
+
+button.addEventListener("dblclick",function(){
+    console.log("Second evenmt listener: Button is clicked");
+});
