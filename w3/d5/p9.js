0153e4e1148fe88fa5864ef99e6345ec3bca8a97
Rohit Raj <rohitrajm254@gmail.com>
2026-03-12 14:35:58 +0530

    Limiting listener to once only for a event

---
+++ w3/d5/p9.js
@@ -0,0 +1,12 @@
+//Limiting listener to once only for a event
+const onceBtn = document.getElementById("onceBtn");
+onceBtn.addEventListener("click",function(){
+    console.log("This click listener works only once.");
+},{once:true});
+//Global keyboard shortcuts creation
+document.addEventListener("keydown",function(event){
+    if(event.ctrlKey && event.key.toLowerCase()==="s"){
+        event.preventDefault();
+        console.log("Custom ctrl+s shortcut triggered");
+    }
+});
