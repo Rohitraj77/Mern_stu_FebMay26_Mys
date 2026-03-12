96f668455619ab04841b4c43832deb5045bd9409
Rohit Raj <rohitrajm254@gmail.com>
2026-03-12 14:49:44 +0530

    finally block usage

---
+++ w4/d1/p6.js
@@ -0,0 +1,34 @@
+// //finally
+// function example(){
+//     try{
+//         console.log("Example in try block");
+//         return "TRY_RETURN";
+//     }
+//     finally{
+//         console.log("This is printed !");
+//     }
+// }
+// console.log("Example result: ",example());
+
+//return in catch block and still not finally
+
+function example1(){
+    try{
+    try{
+        throw new Error("New error");
+    }
+    catch(e){
+        console.log("Example 1: Caught error");
+        // return 10;
+    }
+    finally{
+        console.log("Example 1: finally still runs");
+    }
+}
+catch(e){
+console.log("Example 2 outer catch",e.message);
+}
+}
+
+console.log("Example 1: ",example1());
+
