fa5c1de3e208fe11c573b27ad90efd330abd89e9
Rohit Raj <rohitrajm254@gmail.com>
2026-03-17 10:14:54 +0530

    learning about node project manager

---
+++ w5/d1/p5.sh
@@ -0,0 +1,5 @@
+set -e
+mkdir -p node-learning-project
+cd node-learning-project
+npm init -y # node project manager if we use -y it creates everything that is required for the project
+echo "Project created in($pwd)"  #pwd-present working directory
