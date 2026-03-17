9627a014a2ef0c17a1dfd24d1fcfafff662d11e2
Rohit Raj <rohitrajm254@gmail.com>
2026-03-17 16:18:27 +0530

    Introductuion to promises

---
+++ w5/d2/p8.js
@@ -0,0 +1,16 @@
+// Introductuion to promises
+console.log("Program started");
+function getWelcomeMessage(){
+    return new Promise(function(resolve){
+        setTimeout(() => {
+           resolve("Welcome to Promises") 
+        }, 1000);
+    });
+}
+const messagePromise = getWelcomeMessage();
+console.log("Promise created. Result not ready yet.");
+messagePromise.then(function(message){
+    console.log(message);
+});
+
+console.log("Program continues while promise is pending.")
