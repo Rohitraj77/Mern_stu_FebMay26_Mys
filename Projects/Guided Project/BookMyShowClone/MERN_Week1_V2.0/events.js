937f119c8164800942c0d0ff787a552bc2667c68
Rohitraj77 <rohitrajm254@gmail.com>
2026-04-07 09:36:46 +0530

    version 2 of week1 project

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week1_V2.0/events.js
@@ -0,0 +1,19 @@
+// // This file creates and exports custom event emitter instance
+// const EventEmitter = require("events");
+// // Custom event emitter object
+// const bookingEmitter = new EventEmitter();
+
+// module.exports = bookingEmitter;
+
+
+
+
+
+// This file creates and exports a custom 
+// EventEmitter instance
+const EventEmitter = require("events");
+
+//Custom EventEmitter object
+const bookingEmitter = new EventEmitter();
+
+module.exports = bookingEmitter;
---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week1_V2.0/validator.js
@@ -0,0 +1,75 @@
+// // callback based validation functions 
+
+// function validateMovieSelection(movies, movieId, callback) {
+//     const selectedMovie = movies.find(movie => movie.id === movieId);
+//     if(!selectedMovie) {
+//         return callback("Invalid movie selection. choose a valid movie id.",null);
+//     }
+//     callback(null, selectedMovie);
+// }
+
+// function validateTimeSelection(movies, selectedTime, callback) {
+//     const selectedShowTime = movies.showtimes.find((show)=>show.time.toLowerCase() === selectedTime.toLowerCase());
+//     if(!selectedShowTime) {
+//         return callback("Invalid show time selection. choose a valid show time.",null);
+//     }
+//     callback(null, selectedShowTime);
+// }
+
+// function validateSeatCount(seatCount, callback) {
+//     if(isNaN(seatCount) || seatCount <= 0) {
+//         return callback("Invalid seat count. choose a valid seat count.",null);
+//     }
+//     callback(null,  seatCount );
+// }
+
+// module.exports = {
+//     validateMovieSelection,
+//     validateTimeSelection,
+//     validateSeatCount
+// };
+
+
+
+
+
+
+
+
+// Callback based validation functions
+
+function validateMovieSelection(movies,movieId,callback) {
+    const selectedMovie = movies.find((movie)=>movie.id === movieId);
+
+    if (!selectedMovie) {
+        return callback("Invalid movie selection.Choose a valid movie ID.",null);
+    }
+    callback(null,selectedMovie);
+
+}
+
+function validateTimeSelection(movie, selectedTime, callback) {
+  const selectedShowtime = movie.showtimes.find(
+    (show) => show.time.toLowerCase() === selectedTime.toLowerCase()
+  );
+
+  if (!selectedShowtime) {
+    return callback("Invalid time slot selection. Please choose a valid show time.", null);
+  }
+
+  callback(null, selectedShowtime);
+}
+
+
+function validateSeatCount(seatCount,callback) {
+    if (isNaN(seatCount) || seatCount <= 0) {
+        return callback("Invalid seat count.Enter a valid seat count.",null);
+    }
+    callback(null,seatCount);
+}
+
+module.exports = {
+    validateMovieSelection,
+    validateTimeSelection,
+    validateSeatCount
+};
