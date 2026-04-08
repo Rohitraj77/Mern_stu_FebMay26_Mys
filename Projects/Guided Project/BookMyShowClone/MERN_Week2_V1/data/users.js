b847bc70f38ad9a2f7f9374154cb4bec7fecc7c7
Rohitraj77 <rohitrajm254@gmail.com>
2026-04-08 10:07:18 +0530

    users details of the application

---
+++ Projects/Guided Project/BookMyShowClone/MERN_Week2_V1/data/users.js
@@ -0,0 +1,19 @@
+// users details of the application
+const users = [
+    {
+        id:1,
+        name:"Vishnu",
+        email:"gg@gmail.com",
+        password:"V123",
+        role:"user"
+    },
+        {
+        id:2,
+        name:"Admin",
+        email:"a@e.com",
+        password:"admin123",
+        role:"admin"
+    }
+];
+
+module.exports = users;
