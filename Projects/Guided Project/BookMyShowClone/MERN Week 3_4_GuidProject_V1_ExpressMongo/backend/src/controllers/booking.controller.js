b16c155429eaf7f471eb5025baea7414a30fd10b
Rohitraj77 <rohitrajm254@gmail.com>
2026-04-23 15:29:17 +0530

    controller for creation, cancel & get bookings is added

---
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/controllers/booking.controller.js
@@ -0,0 +1,49 @@
+const bookingService = require("../services/booking.service");
+
+// Create booking 
+exports.createBooking = async (req,res,next) => {
+    try{
+        const booking = await bookingService.createBooking(
+            req.user._id,
+            req.body
+        );
+        res.status(201).json({
+            success:true,
+            message:"Booking confirmed.",
+            data:booking,
+        });
+    }
+    catch(error){
+        next(error);
+    }
+};
+
+// Get bookings
+exports.getMyBookings = async (req,res,next) => {
+    try{
+        const bookings = await bookingService.getUserBookings(req.user._id);
+         res.status(201).json({
+            success:true,
+            message:"Booking fetched.",
+            data:booking,
+        });
+    }
+    catch(error){
+        next(error);
+    }
+};
+
+// Cancel bookings
+exports.CancelBookings = async (req,res,next) => {
+    try{
+        await bookingService.CancelBookings(req.params.id,req.user._id);
+         res.status(200).json({
+            success:true,
+            message:"Booking cancelled.",
+            data:booking,
+        });
+    }
+    catch(error){
+        next(error);
+    }
+};
