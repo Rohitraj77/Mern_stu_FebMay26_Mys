266f6ff122d8deb4460c1351c1f7e913806c9f09
Rohitraj77 <rohitrajm254@gmail.com>
2026-04-23 13:00:03 +0530

    Error forwarding method changed

--- Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Booking.js
+++ Projects/Guided Project/BookMyShowClone/MERN Week 3_4_GuidProject_V1_ExpressMongo/backend/src/models/Booking.js
@@ -1,51 +1,53 @@
 const mongoose = require("mongoose");
 const bookingSchema = new mongoose.Schema({
-    userId:{
+    userId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref:"User",
-        required:true,
-        index:true
+        ref: "User",
+        required: true,
+        index: true,
     },
-    showId:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Show",
-        required:true,
-        index:true,
+    showId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Show",
+        required: true,
+        index: true,
     },
-    seats:{
-        type:[String],
-        required:true,
+    seats: {
+        type: [String],
+        required: true,
     },
-    totalSeats:{
-        type:Number,
-        required:true
+    totalSeats: {
+        type: Number,
+        required: true,
     },
-    status:{
-        type:String,
-        enum:["booked","cancelled"],
-        default:"booked",
-        index:true,
+    status: {
+        type: String,
+        enum: ["booked", "cancelled"],
+        default: "booked",
+        index: true,
     },
-    bookingTime:{
-        type:Date,
-        default:Date.now(),
+    bookingTime: {
+        type: Date,
+        default: Date.now(),
     },
 },
-{
-    timestamps:true,
-});
+    {
+        timestamps: true,
+    });
 
-// Add validation
-bookingSchema.pre("save",function(next){
-    if(this.seats.length === 0){
-        return next(new Error("At least one seat must be selected"));
+//Add validation
+bookingSchema.pre("save", function () {
+    if (this.seats.length === 0) {
+        // return next(new Error("At least one seat must be selected"));
+        throw new Error("At least one seat mjust be selected");
     }
-    if(this.totalSeats!=this.seats.length){
-        return next(new Error("Seat count mismatch"));
-    }
-    next();
-});
-// Compound index
-bookingSchema.index({userId:1,showId:1});
+    if (this.totalSeats != this.seats.length) {
+        // return next(new Error("Seat count mismatch"));
+        throw new Error("Seat count mismatch");
 
-module.exports = mongoose.model("Booking",bookingSchema);
+    }
+});
+//Compound index
+bookingSchema.index({ userId: 1, showId: 1 });
+
+module.exports = mongoose.model("Booking", bookingSchema);
