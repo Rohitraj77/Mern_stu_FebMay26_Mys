38b51b68b19f51cdb8ecc24c3b4a388e75322064
Rohitraj77 <rohitrajm254@gmail.com>
2026-05-07 13:49:47 +0530

    Generated and verified OTP

---
+++ w8/d4/p5.js
@@ -0,0 +1,33 @@
+// Generated and verified OTP
+const crypto = require('crypto');
+const otpStore = {};
+function generateOTP(userId){
+
+    const otp = crypto.randomInt(100000,999999)
+        .toString();
+    otpStore[userId] = {
+        otp,
+        expiresAt:Date.now()+10000,
+        attempts: 0
+    };
+    console.log("OTP: ",otp);
+}
+function verifyOTP(userId,enteredOtp){
+    const record = otpStore[userId];
+
+    if(!record){
+        return "No OTP";
+    }
+    if(Date.now()>record.expiresAt) return "Expired";
+    if(record.attempts >= 3) return "Blocked";
+
+    record.attempts++;
+    return record.otp === String(enteredOtp)?"Valid" : "Invalid";
+}
+generateOTP("user1");
+
+const userEnteredOtp =otpStore["user1"].otp;
+setTimeout(() => {
+
+},1000);
+console.log(verifyOTP("user2",userEnteredOtp));
